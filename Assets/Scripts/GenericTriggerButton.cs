5f60d695623fd7aca3920e49049500ddc8eb576c
jxnny <jwieczorek@hof-university.de>
2025-12-14 18:25:29 +0100

    Add: GameFlowManager to swtich between two different office variants

--- Assets/Scripts/GenericTriggerButton.cs
+++ Assets/Scripts/GenericTriggerButton.cs
@@ -3,11 +3,13 @@ using UnityEngine.Events;
 
 public class GenericTriggerButton : MonoBehaviour
 {
+    public string requiredTag = "Player";
     public UnityEvent onTriggered;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("[GenericTriggerButton] TRIGGER from: " + other.name);
+        Debug.Log("TRIGGERED BY: " + other.name);
         onTriggered?.Invoke();
     }
+
 }
