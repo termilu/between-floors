88847fb818dae3cc03c6d385b5ee16d0324c302e
jxnny <jwieczorek@hof-university.de>
2026-01-10 16:51:37 +0100

    Add: Enums for easier work

---
+++ Assets/_Project/Scripts/Data/Enums.cs
@@ -0,0 +1,19 @@
+﻿namespace _Project.Scripts.Data
+{
+    public class Enums 
+    {
+        public enum MovementType 
+        {
+            Teleport,
+            Smooth
+        }
+    }
+
+    public enum AnomalyCategory 
+    {
+        Visual,
+        Audio,
+        Interaction,
+        Event
+    }
+}
