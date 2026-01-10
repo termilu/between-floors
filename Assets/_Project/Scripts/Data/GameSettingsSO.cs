d36af6671fcefdaebaa799fdd9caf38b8c74cacc
jxnny <jwieczorek@hof-university.de>
2026-01-10 16:55:04 +0100

    Add: central GameSettings

---
+++ Assets/_Project/Scripts/Data/GameSettingsSO.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Data
+{
+    [CreateAssetMenu(menuName = "VRAR/Data/Game Settings", fileName = "GameSettings")]
+    public class GameSettingsSO : ScriptableObject
+    {
+        [Header("Highest Floor")]
+        public int maxFloors = 9; //Muss noch besprochen werden
+        
+        [Header("Movement")]
+        public Enums.MovementType defaultMovementType = Enums.MovementType.Teleport;
+        
+        [Header("Feedback")]
+        public float resultDurationSeconds = 2.0f; //hilfreich für Feedback-Animationen etc.
+    }
+}
