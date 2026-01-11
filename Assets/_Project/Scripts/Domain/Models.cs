ed0613e8d77b97a409d72face90db2e37965c0f2
jxnny <jwieczorek@hof-university.de>
2026-01-11 15:04:29 +0100

    Add: GameSession, FloorRuntime and AnomalyInstance as Models for easier workflow

---
+++ Assets/_Project/Scripts/Domain/Models.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using _Project.Scripts.Data;
+
+namespace _Project.Scripts.Domain
+{
+    public sealed class GameSession
+    {
+        public int currentFloor { get; internal set; }
+        public int failCount { get; internal set; }
+        public int Score { get; internal set; }
+        public MovementType movementType { get; internal set; }
+        
+        public int highestFloorReached { get; internal set; } 
+    }
+
+    public sealed class FloorRuntime
+    {
+        public int floorId { get; }
+        public List<AnomalyInstance> activeAnomalies { get; }
+
+        public FloorRuntime(int floorId, List<AnomalyInstance> activeAnomalies)
+        {
+            this.floorId = floorId;
+            this.activeAnomalies = activeAnomalies ?? new List<AnomalyInstance>();
+        }
+
+        public bool hasAnyActiveAnomalies()
+        {
+            for (int i = 0; i < activeAnomalies.Count; i++)
+            {
+                if (activeAnomalies[i].isActive)
+                    return true;
+            }
+            
+            return false;
+        }
+    }
+    
+    public sealed class AnomalyInstance
+    {
+        public string instanceId { get; }
+        public bool isActive { get; internal set; }
+
+        public AnomalyTypeSO anomalyType { get; }
+        public float intensity { get; }
+
+        public AnomalyInstance(
+            string instanceId,
+            bool isActive,
+            AnomalyTypeSO anomalyType,
+            float intensity)
+        {
+            this.instanceId = instanceId;
+            this.isActive = isActive;
+            this.anomalyType = anomalyType;
+            this.intensity = intensity;
+        }
+    }
+}
