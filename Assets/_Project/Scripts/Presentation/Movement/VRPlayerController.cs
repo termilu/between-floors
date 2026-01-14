3efa2a6c7017a418877a7f17f77a88e5b13a2948
jxnny <jwieczorek@hof-university.de>
2026-01-14 19:36:14 +0100

    Add: uncomplete VR player controller

---
+++ Assets/_Project/Scripts/Presentation/Movement/VRPlayerController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using _Project.Scripts.Data;
+
+namespace _Project.Scripts.Presentation.Movement
+{
+    public sealed class VRPlayerController : MonoBehaviour
+    {
+        //TODO: Überarbeiten + IMovementStrategy (TeleportMovemet, SmoothMovement)
+        
+    }
+}
