0613a3f72786d43d460af6375fd00ebec9f73222
jxnny <jwieczorek@hof-university.de>
2026-01-14 19:34:31 +0100

    Add: simple anomaly view

---
+++ Assets/_Project/Scripts/Presentation/AnomalyView.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using _Project.Scripts.Domain;
+
+namespace _Project.Scripts.Presentation
+{
+    public class AnomalyView : MonoBehaviour
+    {
+        [Header("Optional: Renderer/Animator etc.")]
+        [SerializeField] private GameObject root;
+
+        public AnomalyInstance instance { get; private set; }
+
+        public void Setup(AnomalyInstance inst)
+        {
+            instance = inst;
+            ApplyActiveState(inst != null && inst.isActive);
+        }
+
+        public void ApplyActiveState(bool active)
+        {
+            if (root != null) root.SetActive(active);
+            else gameObject.SetActive(active);
+        }
+
+        public void PlayEffect()
+        {
+            //TODO
+        }
+
+        public void StopEffect()
+        {
+            //TODO
+        }
+    }
+}
