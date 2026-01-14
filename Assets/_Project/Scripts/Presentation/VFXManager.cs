60475bd7d5cf389ca3c3a8384efd6a4aef98e4f0
jxnny <jwieczorek@hof-university.de>
2026-01-14 19:33:29 +0100

    Add: simple VFX manager

---
+++ Assets/_Project/Scripts/Presentation/VFXManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using _Project.Scripts.Domain;
+
+namespace _Project.Scripts.Presentation
+{
+    public sealed class VFXManager : MonoBehaviour
+    {
+        private GameContext context;
+
+        public void Initialize(GameContext ctx)
+        {
+            if (context != null)
+                Unsubscribe();
+
+            context = ctx;
+
+            if (context != null)
+                Subscribe();
+        }
+
+        private void OnDestroy()
+        {
+            Unsubscribe();
+        }
+
+        private void Subscribe()
+        {
+            context.events.onDecisionOutcome += OnDecisionOutcome;
+        }
+
+        private void Unsubscribe()
+        {
+            if (context == null) return;
+
+            context.events.onDecisionOutcome -= OnDecisionOutcome;
+        }
+
+        private void OnDecisionOutcome(DecisionOutcomeData data)
+        {
+            if (data.wasCorrect) PlayEffect("Correct");
+            else PlayEffect("Wrong");
+        }
+
+        public void PlayEffect(string effectId)
+        {
+            //TODO: Vielleicht zwei verschiede Geräusche im Aufzug
+        }
+    }
+}
