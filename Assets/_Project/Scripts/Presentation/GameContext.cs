909a0977cb1e89c59892324f950778e233997d5d
jxnny <jwieczorek@hof-university.de>
2026-01-14 13:21:34 +0100

    Add: GameContext for easier workflow

---
+++ Assets/_Project/Scripts/Presentation/GameContext.cs
@@ -0,0 +1,24 @@
+﻿using _Project.Scripts.Domain;
+
+namespace _Project.Scripts.Presentation
+{
+    public sealed class GameContext
+    {
+        public GameEventsHub events { get; }
+        public GameStateMachine stateMachine { get; }
+        public GameFlowService flow { get; }
+        public AnomalyService anomalies { get; }
+
+        public GameContext(
+            GameEventsHub events,
+            GameStateMachine stateMachine,
+            GameFlowService flow,
+            AnomalyService anomalies)
+        {
+            this.events = events;
+            this.stateMachine = stateMachine;
+            this.flow = flow;
+            this.anomalies = anomalies;
+        }
+    }
+}
