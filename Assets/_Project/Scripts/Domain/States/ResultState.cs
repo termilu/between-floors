b5b37d7e2b5c2258a0edb4d5734adbad8ad9967a
jxnny <jwieczorek@hof-university.de>
2026-01-15 17:48:58 +0100

    Fix: Extend ResultState constructor to use decision outcome for next state

--- Assets/_Project/Scripts/Domain/States/ResultState.cs
+++ Assets/_Project/Scripts/Domain/States/ResultState.cs
@@ -5,17 +5,24 @@
         private readonly GameStateMachine stateMachine;
         private readonly GameFlowService flow;
         
+        private readonly int nextFloor;
+        private readonly bool isGameOver;
+        
         private float remainingSeconds;
 
-        public ResultState(GameStateMachine stateMachine, GameFlowService flow)
+        public ResultState(GameStateMachine stateMachine, GameFlowService flow, float durationSeconds, int nextFloor,
+            bool isGameOver)
         {
             this.stateMachine = stateMachine;
             this.flow = flow;
+            this.nextFloor = nextFloor;
+            this.isGameOver = isGameOver;
+
+            remainingSeconds = durationSeconds;
         }
 
         public void Enter()
         {
-            remainingSeconds = 2.0f;
         }
         
         public void Exit() { }
@@ -28,6 +35,9 @@
             {
                 stateMachine.changeState(new ExploreState(stateMachine, flow));
             }
+            
+            flow.StartFloor(nextFloor);
+            stateMachine.changeState(new ExploreState(stateMachine, flow));
         }
     }
 }
